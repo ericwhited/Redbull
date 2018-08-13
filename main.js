520d4659eb74060de7fb87d7dfd1e3bad2e0ab23
Eric Whited <whited.j.eric@gmail.com>
2018-08-13 16:07:08 -0400

    added search bar and slide in

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+//Yellow search bar drop down
+$(document).ready("#yellowSearchIcon").click(function(){
+    $('#searchBar').slideDown(200);
+});
+
+//add actual search functionality
+
+
+
+// Home page call for all items
 $.ajax({
   type: 'get',
   url: 'https://www.redbullshopus.com/products.json',
@@ -8,5 +18,42 @@ $.ajax({
     	 + "<div class='product'>" + response.products[i].title + "</div>"
     	 + "</div>");
 	}
-})
+});
+
+
+
+
+// Call for mens items and places them into mens product container on mens page.
+$.ajax({
+  type: 'get',
+  url: 'https://www.redbullshopus.com/products.json',
+  success: function(response) {
+    for(i=0; i<response.products.length; i++)
+      if(response.products[i].tags.includes("Men")) {
+    	$('#mensContainer').append("<div class='wrap'>"
+    	 + "<img class='productPhotos' src=" + (response.products[i].images[0].src) + ">" 
+    	 + "<div class='product'>" + response.products[i].title + "</div>"
+    	 + "</div>");  
+      }
+  }
+});
+
+
+
+// Womens product call
+$.ajax({
+  type: 'get',
+  url: 'https://www.redbullshopus.com/products.json',
+  success: function(response) {
+    for(i=0; i<response.products.length; i++)
+      if(response.products[i].tags.includes("Women")) {
+    	$('#womensContainer').append("<div class='wrap'>"
+    	 + "<img class='productPhotos' src=" + (response.products[i].images[0].src) + ">" 
+    	 + "<div class='product'>" + response.products[i].title + "</div>"
+    	 + "</div>");  
+      }
+  }
+});
+
+
 
