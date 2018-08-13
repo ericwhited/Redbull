944b1b4043d220d38a62f2d4a9848423ecd4370a
Eric Whited <whited.j.eric@gmail.com>
2018-08-13 11:13:04 -0400

    fixed full sized layout to display products correctly & others tuff

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ $.ajax({
   url: 'https://www.redbullshopus.com/products.json',
   success: function(response) {
     for(i=0; i < response.products.length; i++)
-		$('#container').append("<img class='productPhotos' src=" + (response.products[i].images[0].src) + ">"),
-		$('#container').append("<div class='product'>" + response.products[i].title + "</div>");
-}
+    	$('#container').append("<div class='wrap'>"
+    	 + "<img class='productPhotos' src=" + (response.products[i].images[0].src) + ">" 
+    	 + "<div class='product'>" + response.products[i].title + "</div>"
+    	 + "</div>");
+	}
 })
+
